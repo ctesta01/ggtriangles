757ef5dd424c07fb91488c680773916f4b640434
Christian Testa <testa101@gmail.com>
2022-02-05 14:03:00 -0500

    updated syntax using triangle_height, triangle_width; adding documentation

--- R/geom_triangles.R
+++ R/geom_triangles.R
@@ -1,21 +1,46 @@
 
+#' Draw Triangle Legend Key with polygonGrob
+#'
+#' @export
+#'
+#' @examples
+#' library(grid)
+#' grid.newpage()
+#'
+#' triangle <-
+#'   draw_key_triangle(
+#'     data = data.frame(
+#'       x = .5,
+#'       y = .01,
+#'       triangle_height = -.5,
+#'       triangle_width = .5,
+#'       angle = 0,
+#'       fill = 'black',
+#'       alpha = 1,
+#'       colour = 'black',
+#'       size = 1
+#'     )
+#'   )
+#'
+#' grid.draw(triangle)
 draw_key_triangle <- function(data, params, size) {
   idx <- rep(seq_len(nrow(data)), each = 3)
   rep_data <- data[idx, ]
 
   x_off <- as.vector(outer(
     c(-0.5, 0, 0.5),
-    data$width
+    data$triangle_width
   ))
 
   y_off <- as.vector(outer(
     c(0, 1, 0),
-    data$height
+    data$triangle_height
   ))
 
   ang <- rep_data$angle * (pi / 180)
   x_new <- x_off * cos(ang) - y_off * sin(ang)
   y_new <- x_off * sin(ang) + y_off * cos(ang)
+
 
   # Origin x and y have fixed values
   x <- unit(0.5, "npc") + unit(x_new, "cm")
@@ -33,26 +58,33 @@ draw_key_triangle <- function(data, params, size) {
 }
 
 
-
+#' GeomTriangles ggproto - draws triangles with base having midpoint (x,y)
+#'
+#' @export
 GeomTriangles <- ggproto(
   "GeomTriangles", GeomPoint,
   default_aes = aes(
     colour = "black", fill = "black", size = 0.5, linetype = 1,
-    alpha = 1, angle = 0, width = 0.5, height = 0.5
+    alpha = 1, angle = 0, triangle_width = 0.5, triangle_height = 0.5
   ),
-
+  setup_params = function(data, params) {
+    print(params)
+    return(params)
+  },
   draw_panel = function(
     data, panel_params, coord, na.rm = FALSE
   ) {
+
     # Apply coordinate transform
     df <- coord$transform(data, panel_params)
 
     # Repeat every row 3x
     idx <- rep(seq_len(nrow(df)), each = 3)
     rep_df <- df[idx, ]
+
     # Calculate offsets from origin
-    x_off <- as.vector(outer(c(-0.5, 0, 0.5), df$width))
-    y_off <- as.vector(outer(c(0, 1, 0), df$height))
+    x_off <- as.vector(outer(c(-0.5, 0, 0.5), df$triangle_width))
+    y_off <- as.vector(outer(c(0, 1, 0), df$triangle_height))
 
     # Rotate offsets
     ang <- rep_df$angle * (pi / 180)
@@ -73,7 +105,8 @@ GeomTriangles <- ggproto(
       )
     )
   },
-  draw_key = draw_key_triangle
+  draw_key = draw_key_triangle,
+  setup_data = function(data, params) { return(data) }
 )
 
 
@@ -89,22 +122,39 @@ GeomTriangles <- ggproto(
 #'     aes(
 #'       x = Sepal.Length,
 #'       y = Sepal.Width,
-#'       height = Petal.Length,
+#'       triangle_height = Petal.Length,
 #'       fill = Petal.Length,
-#'       color = Species,
-#'       width = Petal.Width
+#'       color = Petal.Length,
+#'       triangle_width = Petal.Width
 #'     )
 #'   ) +
 #'   geom_triangles(alpha = .7, size = 1) +
-#'   scale_fill_viridis_c(end = .8) +
-#'   scale_color_viridis_d(option = 'A', end = .8) +
-#'   scale_width_continuous(range = c(0.1, 1), n.breaks = 4) +
-#'   scale_height_continuous(range = c(0.1, 1), n.breaks = 3) +
+#'   scale_fill_viridis_c(option = 'A', end = .8) +
+#'   scale_color_viridis_c(option = 'A', end = .8) +
+#'   scale_triangle_width_continuous(range = c(0.1, 1), n.breaks = 4) +
+#'   scale_triangle_height_continuous(range = c(0.1, 1), n.breaks = 3) +
 #'   ggtitle(
 #'     "Sepal length and width and petal length and width of iris flowers",
 #'     "Petal length and width are shown by the height and width of each triangle"
 #'   ) +
-#'   theme_bw()
+#'   theme_bw() +
+#'   theme(legend.position = 'bottom')
+#'
+#' # example not currently working:
+#' ggplot(datasets::sleep, aes(
+#'   x = as.numeric(ID),
+#'   y = as.numeric(group),
+#'   triangle_height = extra
+#'   # angle = ifelse(extra > 0, 0, 180) # this is the crude way of setting appropriate
+#'   # angle for negative numbers that I'd like to avoid using, but it does work
+#' )) +
+#'   geom_triangles() +
+#'   scale_y_continuous(breaks = c(1,2)) +
+#'   expand_limits(y = c(0.5, 2.5)) +
+#'   xlab("Individual") +
+#'   ylab("Drug Given") +
+#'   ggtitle("Data show the effects of two soporific drugs administered to a group of 10 people") +
+#'   labs(caption = "Data from datasets::sleep")
 #'
 geom_triangles <- function(mapping = NULL, data = NULL,
                            position = "identity", na.rm = FALSE, show.legend = NA,
@@ -116,19 +166,31 @@ geom_triangles <- function(mapping = NULL, data = NULL,
   )
 }
 
-scale_width_continuous <- function(..., range = c(0.1, 1)) {
+#' Alter triangle width scaling
+#'
+#' Using scales::rescale_pal, scale triangle width to appear with the given
+#' range.
+#'
+#' @export
+scale_triangle_width_continuous <- function(..., range = c(0.1, 1)) {
   continuous_scale(
-    aesthetics = "width",
-    scale_name = "wscale",
+    aesthetics = "triangle_width",
+    scale_name = "triangle_wscale",
     palette = scales::rescale_pal(range),
     ...
   )
 }
 
-scale_height_continuous <- function(..., range = c(0.1, 1)) {
+#' Alter triangle height scaling
+#'
+#' Using scales::rescale_pal, scale triangle height to appear with the given
+#' range.
+#'
+#' @export
+scale_triangle_height_continuous <- function(..., range = c(0.1, 1)) {
   continuous_scale(
-    aesthetics = "height",
-    scale_name = "hscale",
+    aesthetics = "triangle_height",
+    scale_name = "triangle_hscale",
     palette = scales::rescale_pal(range),
     ...
   )
